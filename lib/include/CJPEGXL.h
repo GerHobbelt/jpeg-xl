aaa292ff1dee45d577f8a8d684d647f20f7dd9ff
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-06-28 11:07:22 +0800

    [feat] add swift wrapper

---
+++ lib/include/CJPEGXL.h
@@ -0,0 +1,43 @@
+/*===----------------------------------------------------------------------===*/
+/*                                                        ___   ___           */
+/* CJPEGXL.h                                            /'___\ /\_ \          */
+/*                                                     /\ \__/ \//\ \         */
+/* Author: Fang Ling (fangling@fangl.ing)              \ \ ,__\  \ \ \        */
+/* Date: June 28, 2025                                  \ \ \_/__ \_\ \_  __  */
+/*                                                       \ \_\/\_\/\____\/\_\ */
+/* Copyright (c) 2025-2025 Fang Ling All Rights Reserved. \/_/\/_/\/____/\/_/ */
+/*===----------------------------------------------------------------------===*/
+
+#ifndef CJPEGXL_h
+#define CJPEGXL_h
+
+#include "jxl/cms.h"
+#include "jxl/cms_interface.h"
+#include "jxl/codestream_header.h"
+#include "jxl/color_encoding.h"
+#include "jxl/compressed_icc.h"
+#include "jxl/decode.h"
+#include "jxl/encode.h"
+#include "jxl/gain_map.h"
+#include "jxl/jxl_cms_export.h"
+#include "jxl/jxl_export.h"
+#include "jxl/jxl_threads_export.h"
+#include "jxl/memory_manager.h"
+#include "jxl/parallel_runner.h"
+#include "jxl/resizable_parallel_runner.h"
+#include "jxl/stats.h"
+#include "jxl/thread_parallel_runner.h"
+#include "jxl/types.h"
+#include "jxl/version.h"
+
+#endif /* CJPEGXL_h */
+
+/*===----------------------------------------------------------------------===*/
+/*         ____     _____  ____    ____    ____    __   __   __               */
+/*        /\  _`\  /\___ \/\  _`\ /\  _`\ /\  _`\ /\ \ /\ \ /\ \              */
+/*        \ \ \/\_\\/__/\ \ \ \L\ \ \ \L\_\ \ \L\_\ `\`\/'/'\ \ \             */
+/*         \ \ \/_/_  _\ \ \ \ ,__/\ \  _\L\ \ \L_L`\/ > <   \ \ \  __        */
+/*          \ \ \L\ \/\ \_\ \ \ \/  \ \ \L\ \ \ \/, \ \/'/\`\ \ \ \L\ \       */
+/*           \ \____/\ \____/\ \_\   \ \____/\ \____/ /\_\\ \_\\ \____/       */
+/*            \/___/  \/___/  \/_/    \/___/  \/___/  \/_/ \/_/ \/___/        */
+/*===----------------------------------------------------------------------===*/
